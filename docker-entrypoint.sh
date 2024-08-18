dd04d5186734953345c9cee6b45d97dc781e38e9
Nick Tyler <nicholas.s.tyler.4@gmail.com>
2024-08-18 23:19:51 +0000

    see slurm logs in github

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,19 +5,17 @@ echo "---> Starting the MUNGE Authentication service (munged) ..."
 gosu munge /usr/sbin/munged
 
 echo "---> Starting the slurmctld ..."
-exec gosu slurm /usr/sbin/slurmctld -i -Dvvv > slurmctld.log 2>&1 &
+exec gosu slurm /usr/sbin/slurmctld -i -Dvvv &
 
 echo "---> Waiting for slurmctld to become active before starting slurmd..."
 
 echo "---> Starting the Slurm Node Daemon (slurmd) ..."
-exec /usr/sbin/slurmd -Dvvv > slurmd.log 2>&1 &
+exec /usr/sbin/slurmd -Dvvv &
 
 if [ "$#" -eq 0 ]; then
     cd /home/hpcuser
     exec gosu hpcuser /bin/bash -l
 fi
 echo "---> Running user command '${@}'"
-tail -f slurmctld.log &
-tail -f slurmd.log &
 exec gosu hpcuser "$@"
 
