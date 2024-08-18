4fc1829af7302fa2a0e77607540e0c6a09daef84
Nick Tyler <nicholas.s.tyler.4@gmail.com>
2024-08-18 22:26:33 +0000

    Allow commands from entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -13,4 +13,8 @@ echo "---> Starting the Slurm Node Daemon (slurmd) ..."
 exec /usr/sbin/slurmd -Dvvv > slurmd.log 2>&1 &
 
 cd /home/hpcuser
-exec gosu hpcuser /bin/bash -l
+if [ "$#" -eq 0 ]; then
+    exec gosu hpcuser /bin/bash -l
+fi
+echo "---> Running user command '${@}'"
+exec gosu hpcuser "$@"
